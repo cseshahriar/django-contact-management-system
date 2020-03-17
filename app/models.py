d3a68b694b99baef44f17d3043d3f87d1338c87b
cseshahriar <cses.hahriar.hosen@gmail.com>
2020-03-17 23:46:34 +0600

    contat model and media settings

--- app/models.py
+++ app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+# contact model 
+class Contact(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254)
+    phone = models.IntegerField()
+    info = models.CharField(max_length=30)
+    gender = models.CharField(max_length=50, choices=(
+        ('male', 'Male'),
+        ('female', 'Female')
+    ))
+    image = models.ImageField(upload_to='images/', blank=True)
+    created_at = models.DateField(auto_now_add=True)  
+
+    def __str__(self):
+        return self.name 
+    
