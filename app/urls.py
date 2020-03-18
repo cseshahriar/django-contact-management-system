54980b71d275e53f28c9758363167ab7f56f1f8c
cseshahriar <cses.hahriar.hosen@gmail.com>
2020-03-18 22:15:54 +0600

    simple searching

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
-from app.views import ContactList, ContactDetailView
+from . views import ContactList, ContactDetailView
 
 urlpatterns = [
     path('', ContactList.as_view(), name='home'), 
-    path('detail/<int:pk>/', ContactDetailView.as_view(), name='single')    
+    path('detail/<int:pk>/', ContactDetailView.as_view(), name='single'),
+    path('search/', views.search, name='search'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect   
 from django.views.generic import ListView, DetailView
 from app.models import Contact
+
 
 # class base view 
 class ContactList(ListView):
@@ -12,3 +13,16 @@ class ContactDetailView(DetailView):
     template_name = 'detail.html'
     model = Contact 
     context_object_name = 'contact'
+
+
+def search(request):
+    if request.GET:
+        search_term = request.GET['search_term']
+        search_results = Contact.objects.filter(name__icontains=search_term)  
+        context = {  
+            'search_term': search_term,
+            'contacts': search_results 
+        }   
+        return render(request, 'search.html', context)  
+    else:
+        return redirect('home') # url name 
