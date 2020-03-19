8732e11efb115ae12aad1adad31a2bebead23d44
cseshahriar <cses.hahriar.hosen@gmail.com>
2020-03-19 23:48:31 +0600

    login and logout

--- contacts/settings.py
+++ contacts/settings.py
@@ -129,3 +129,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4' 
+
+# for authentication 
+LOGIN_URL = 'login'
+LOGOUT_URL = 'logout'
+LOGIN_REDIRECT_URL = 'home' 
